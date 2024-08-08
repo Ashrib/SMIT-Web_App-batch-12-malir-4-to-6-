3ec26db498e692e1577b80b09cbe20711e590bed
Ashrib <asharib920@gmail.com>
2024-08-08 18:43:27 +0500

    variables

--- JS/app.js
+++ JS/app.js
@@ -1,3 +1,48 @@
-alert("welcome");
-alert("instructions");
-window.alert("instructions");
+// alert("welcome");
+// window.alert("instructions");
+
+var myName = "Mark";
+console.log(myName)
+
+myName = "Ace";
+console.log(myName);
+
+var greeting;
+greeting = "Welcome to my website";
+
+
+var name = myName;
+
+var my_name;
+
+
+var num = 50 ;
+var num2 = 50;
+
+var total = num + num2; 
+var userInp = 85;
+var per = userInp / total * 100; 
+
+console.log(per)
+
+document.write(per)
+
+
+var num1 = "10";
+
+var num3 = num1 - 5;
+console.log(num3)
+
+console.log("my name" + " asharib")
+
+var data , num2, num_, num$;
+alert(2+2)
+
+
+
+
+
+
+
+
+
