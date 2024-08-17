2fe4038dba174dc21a68e7bb7dfc66258647163d
Ashrib <asharib920@gmail.com>
2024-08-17 18:01:01 +0500

    math expressions, prompt

--- JS/app.js
+++ JS/app.js
@@ -2,47 +2,68 @@
 // window.alert("instructions");
 
 var myName = "Mark";
-console.log(myName)
+
+console.log(myName);
 
 myName = "Ace";
 console.log(myName);
 
 var greeting;
-greeting = "Welcome to my website";
 
+greeting = "Welcome to my website";
 
 var name = myName;
 
 var my_name;
 
-
-var num = 50 ;
+var num = 50;
 var num2 = 50;
 
-var total = num + num2; 
+var total = num + num2;
 var userInp = 85;
-var per = userInp / total * 100; 
+var per = (userInp / total) * 100;
 
-console.log(per)
+console.log(per);
 
-document.write(per)
-
+document.write(per);
 
 var num1 = "10";
 
 var num3 = num1 - 5;
-console.log(num3)
+console.log(num3);
 
-console.log("my name" + " asharib")
+console.log("my name" + " asharib");
 
-var data , num2, num_, num$;
-alert(2+2)
+var data, num2, num_, num$;
+// alert(2+2)
 
+var num1 = 10;
+var num2 = 5;
+var num3 = 2;
+var result;
+//10    +   12   +   13   - 5      +   3
+// result = num1++ + ++num1 + ++num1 - num2-- + --num2/2
 
+result = 2 * "2";
+console.log(result);
 
+UserName = "anyone ";
 
+// alert("Thank you " + UserName + "for coming")
 
+var number = 5;
+// number = number + 1
+// number = number - 1
 
+console.log(++number);
 
+number++;
+number++;
+console.log(number);
 
+var totalCost = ((1 + 3) * 4) / 7;
+console.log(totalCost);
 
+var inp = prompt("Your species?", "human");
+inp  =  inp + 5  // if take number as input it's data-type will be string
+console.log(inp);
