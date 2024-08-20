857f42d69014b7eb552ceb4defe07de4e81398b7
manaheel jamil <manaheeljamil20@gmail.com>
2024-08-20 17:59:51 +0500

    if, else if statements

---
+++ JS/if statements/app.js
@@ -0,0 +1,100 @@
+console.log("hello");
+
+var age = 17;
+
+if(age == 18)
+{
+
+    alert("Welcome");
+    alert("Welcome");
+    alert("Welcome");
+}
+
+
+if("18" === 18) //  not equal
+{
+
+    alert("true");
+
+}
+if("18" == 18)  // equal
+{
+
+    alert("true");
+
+}
+
+var inp = prompt("enter your age");
+
+
+
+if(inp === 18)
+{
+    var num = inp + 10 ;
+    alert("true");
+
+}
+
+if(15 !== 17)
+{
+ alert("not equal")
+
+}
+
+
+if(5 < 7){
+ console.log("true")
+}
+
+if(5 > 7){
+    console.log("true")
+}
+
+if(5 >= 5){
+    console.log("5 >= 7")
+}
+
+if(5 <= 6){
+    console.log("5 <= 6")
+    
+}
+
+
+var x = "Vatican";
+
+
+if(x === "Vaticans")
+{
+    alert("Correct!");
+    alert("Correct!");
+    
+}
+else{
+    alert("Wrong answer");
+}
+
+
+
+
+var total = 80;
+
+if(total > 79 ){
+    alert("A+")
+}
+else if(total >= 70){
+    alert("B+")
+}
+else if(total >= 70){
+    alert("B+")
+}
+else if(total >= 70){
+    alert("B+")
+}
+else{
+    alert("C")
+}
+
+
+
+
+
