27eddb463b0b0a981ced601c8e7a749816c7e2b4
manaheel jamil <manaheeljamil20@gmail.com>
2024-08-22 18:32:11 +0500

    if else

--- JS/if statements/app.js
+++ JS/if statements/app.js
@@ -1,99 +1,174 @@
-console.log("hello");
+// console.log("hello");
 
-var age = 17;
+// var age = 17;
 
-if(age == 18)
-{
+// if(age == 18)
+// {
 
-    alert("Welcome");
-    alert("Welcome");
-    alert("Welcome");
-}
-
-
-if("18" === 18) //  not equal
-{
-
-    alert("true");
-
-}
-if("18" == 18)  // equal
-{
-
-    alert("true");
-
-}
-
-var inp = prompt("enter your age");
+//     alert("Welcome");
+//     alert("Welcome");
+//     alert("Welcome");
+// }
 
 
+// if("18" === 18) //  not equal
+// {
 
-if(inp === 18)
-{
-    var num = inp + 10 ;
-    alert("true");
+//     alert("true");
 
-}
+// }
+// if("18" == 18)  // equal
+// {
 
-if(15 !== 17)
-{
- alert("not equal")
+//     alert("true");
 
-}
+// }
+
+// var inp = prompt("enter your age");
 
 
-if(5 < 7){
- console.log("true")
-}
 
-if(5 > 7){
-    console.log("true")
-}
+// if(inp === 18)
+// {
+//     var num = inp + 10 ;
+//     alert("true");
 
-if(5 >= 5){
-    console.log("5 >= 7")
-}
+// }
 
-if(5 <= 6){
-    console.log("5 <= 6")
+// if(15 !== 17)
+// {
+//  alert("not equal")
+
+// }
+
+
+// if(5 < 7){
+//  console.log("true")
+// }
+
+// if(5 > 7){
+//     console.log("true")
+// }
+
+// if(5 >= 5){
+//     console.log("5 >= 7")
+// }
+
+// if(5 <= 6){
+//     console.log("5 <= 6")
     
+// }
+
+
+// var x = "Vatican";
+
+
+// if(x === "Vaticans")
+// {
+//     alert("Correct!");
+//     alert("Correct!");
+    
+// }
+// else{
+//     alert("Wrong answer");
+// }
+
+
+
+
+// var total = 80;
+
+// if(total > 79 ){
+//     alert("A+")
+// }
+// else if(total >= 70){
+//     alert("B+")
+// }
+// else if(total >= 70){
+//     alert("B+")
+// }
+// else if(total >= 70){
+//     alert("B+")
+// }
+// else{
+//     alert("C")
+// }
+
+
+
+
+
+
+
+// set of conditions
+
+var weight = 60;
+var age = 18;
+
+// if(weight >= 60 && age >= 18){
+//     alert("allow")
+// }
+// else{
+//     alert("not allow")
+
+// }
+var gender = "male";
+
+// if(weight >= 60 || age >= 19 || gender === "male"){
+//     alert("allow")
+// }
+// else{
+//     alert("not allow")
+
+// }
+
+
+
+if((weight >= 60 && age >= 19) && (gender === "male" || 6 === 5)){
+    // alert("allow")
+}
+else{
+    // alert("not allow")
+
 }
 
 
-var x = "Vatican";
 
 
-if(x === "Vaticans")
+
+if((weight === 60  && age>17 ) && ( gender === "male"))
 {
-    alert("Correct!");
-    alert("Correct!");
+ 
+    alert("Ok");
+
+}
+
+
+
+
+if(age>= 18)
+{
+   if(gender === "male"){
+       alert("web and app")
+   }
+   else{
+       alert("graphic")
+   }
     
 }
 else{
-    alert("Wrong answer");
-}
+    if(age>=12 && gender === "male" ){
+        alert("techno kids")
+    }
+    else if(age>=8){
+        alert("simple computer")
+    }
+    else{
+        alert("not allowed")
+    }
 
 
-
-
-var total = 80;
-
-if(total > 79 ){
-    alert("A+")
 }
-else if(total >= 70){
-    alert("B+")
-}
-else if(total >= 70){
-    alert("B+")
-}
-else if(total >= 70){
-    alert("B+")
-}
-else{
-    alert("C")
-}
-
 
 
 
