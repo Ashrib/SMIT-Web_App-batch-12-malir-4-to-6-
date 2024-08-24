63aae3effcabca7f93c64e75f461ddfc03a90107
Ashrib <asharib920@gmail.com>
2024-08-24 18:30:41 +0500

    array

---
+++ JS/arrays/app.js
@@ -0,0 +1,38 @@
+
+
+var cities = ["karachi", "Lahore", "Islamabad"];
+
+console.log(cities[0])
+console.log(cities[1])
+// alert("Welcome to " + cities[1])
+
+
+var mixedValues = ["karachi", 50, true];
+console.log(mixedValues);
+
+mixedValues = [5 , 6];
+
+console.log(mixedValues);
+
+mixedValues[1] =  "hello";
+mixedValues[2] =  10;
+console.log(mixedValues);
+
+mixedValues.pop(); // remove at the end
+console.log(mixedValues);
+
+mixedValues.push("hello", "hi",60); // add one or more at the end
+console.log(mixedValues);
+
+mixedValues.shift() // remove from beginning
+console.log(mixedValues);
+
+mixedValues.unshift("10", 15) // add one or move from beginning
+console.log(mixedValues);
+
+
+
+// q1
+alert()
+
+// q2
