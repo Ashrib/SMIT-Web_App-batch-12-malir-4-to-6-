4c5d3c9fc445cde66161dde7279fe359a1d125c8
aliyanshaikh66 <aliyanzahid02@gmail.com>
2024-08-29 18:25:19 +0500

    for loop

---
+++ JS/for loops/app.js
@@ -0,0 +1,22 @@
+
+for(var i = 0; i <= 9; i++){
+
+    console.log("hello", i ,i+1 );
+}
+
+// console.log("hello");
+
+var cityArray = ["karachi",  "lahore", "islamabad"];
+var checkCity = "lahore";
+
+
+for(var i = 0; i < cityArray.length; i++ ){
+    
+    console.log(i)
+    if(checkCity === cityArray[i]){
+        alert("yes "+ "index: " + i)
+        break;
+    }
+
+    "2 X 2 = 4"
+}
