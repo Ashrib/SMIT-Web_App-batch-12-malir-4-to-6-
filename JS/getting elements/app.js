9c171640e2b48a8ffcc61331cc9ee82ebeb7b6ac
Ashrib <asharib920@.com>
2024-09-14 20:06:29 +0500

    random number and getting elements

--- JS/getting elements/app.js
+++ JS/getting elements/app.js
@@ -3,21 +3,37 @@ var text = "there is some text"
 
 var heading = document.getElementById("heading1");
 var headings = document.getElementsByClassName("h2");
-// console.log(heading)
-console.log(headings[1])
+console.log(heading);
+console.log(headings[1]);
 
 
 var headingText = heading.innerText;
-console.log(headingText)
+console.log(headingText);
 
 
 heading.innerText = "world";
 
 headings[1].innerText = text;
 
-var userInp = prompt("enter your name")
-var para = document.getElementById("p1")
+var userInp = prompt("enter your name");
+var para = document.getElementById("p1");
 para.innerText = userInp;
 
       
-console.log(document.getElementById("h3").innerHTML)
+console.log(document.getElementById("h3").innerHTML);
+
+
+
+
+var getSpan = document.getElementById("span1");
+
+
+
+
+var getBody = document.getElementsByTagName("h1");
+console.log(getBody[0])
+
+
+
+
+
---
+++ JS/random numbers/app.js
@@ -0,0 +1,26 @@
+
+
+// var randomNum = Math.random() * 100000000000000000;
+// var randomNum = Math.floor((Math.random() * 10)+1);
+
+// console.log(randomNum)
+
+// var userMarks = prompt("enter your marks");
+
+// var per = (userMarks * 100)/850;
+// console.log(per);
+// console.log(userMarks );
+
+console.log(parseInt("1.999"));
+console.log(parseFloat("1.999"));
+
+
+console.log(Number("38"));
+
+var num = 100;
+num = num.toString();
+console.log(typeof num, num);
+
+
+
+
